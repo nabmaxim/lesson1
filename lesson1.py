72691f5abbb3d71c8a9c45a4e9db36a4641351ef
nabmaxim <nabmaxim@gmail.com>
2024-11-07 00:46:18 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+print(9**0.5) #1st program
+print (9.99>9.98 and 1000 != 1000.1) #2st program
+print (2*2+2)# 3rd program
+print (2*(2+2)) #4th program
+print ((2*2+2) == 2*(2+2))
+print(int(float('123.456') * 10) % 10)
